d449cdcf6570845bbcc847f443a696b20c8c65f3
Klimonov <22pov22@gmail.com>
2018-05-20 13:55:07 +0300

    exercise 02

--- exercises.rkt
+++ exercises.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-;01.Define a procedure sum-of-squares-of-top-two that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
+;01.Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
 (define (sum-of-squares-of-top-two x y z) 
     (define (sum-of-square a b) (+ (* a a) (* b b)))
     (cond ((and (< x y) (< x z)) (sum-of-square y z))
@@ -9,3 +9,18 @@
     )
 )
 (provide sum-of-squares-of-top-two)
+
+#| 02.Implement the Ackermann's function A. It takes two parameters, x and y, and works as follows:
+if y = 0, then it returns 0;
+if x = 0, then it returns 2*y;
+if y = 1, then it returns 2;
+else, it calls itself (function A) with x = x-1 and y = A ( x, (y - 1) ); |#
+(define (A x y) 
+    (cond ((= y 0) 0)
+          ((= x 0) (* 2 y))
+          ((= y 1) 2)
+          (else (A (- x 1) 
+                    (A x (- y 1))))
+    )
+)
+(provide A)
--- tests.rkt
+++ tests.rkt
@@ -6,3 +6,8 @@
 (check-equal? (sum-of-squares-of-top-two 1 2 3) 13)
 (check-equal? (sum-of-squares-of-top-two 3 4 2) 25)
 (check-equal? (sum-of-squares-of-top-two 5 3 4) 41)
+
+;Exercise 02
+(check-equal? (A 1 10) 1024)
+(check-equal? (A 2 4) 65536)
+(check-equal? (A 3 3) 65536)
