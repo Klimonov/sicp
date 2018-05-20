8adb1d98d232ddcfdf3bc0bea9043f9e65f225db
Klimonov <22pov22@gmail.com>
2018-05-20 17:08:40 +0300

    Exercise 1.12

--- exercises.rkt
+++ exercises.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-;01.Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
+;Exercise 1.3. Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
 (define (sum-of-squares-of-top-two x y z) 
     (define (sum-of-square a b) (+ (* a a) (* b b)))
     (cond ((and (< x y) (< x z)) (sum-of-square y z))
@@ -10,7 +10,7 @@
 )
 (provide sum-of-squares-of-top-two)
 
-#| 02.Implement the Ackermann's function A. It takes two parameters, x and y, and works as follows:
+#| Exercise 1.10. Implement the Ackermann's function A. It takes two parameters, x and y, and works as follows:
 if y = 0, then it returns 0;
 if x = 0, then it returns 2*y;
 if y = 1, then it returns 2;
@@ -24,3 +24,26 @@ else, it calls itself (function A) with x = x-1 and y = A ( x, (y - 1) ); |#
     )
 )
 (provide A)
+
+#| Exercise 1.11.  A function f is defined by the rule that f(n) = n if n<3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n> 3. Write a procedure that computes f by means of a recursive process. Write a procedure that computes f by means of an iterative process. |#
+(define (f n)
+    (cond ((< n 3) n)
+        (else (+ (f (- n 1)) 
+                 (* 2 (f (- n 2))) 
+                 (* 3 (f (- n 3)))) 
+        )
+    )
+)
+(provide f)
+
+#| Exercise 1.12.  The following pattern of numbers is called Pascal's triangle.
+The numbers at the edge of the triangle are all 1, and each number inside the triangle is the sum of the two numbers above it.35 Write a procedure that computes elements of Pascal's triangle by means of a recursive process. |#
+(define (pascal-triangle row column)
+    (cond ((= 1 column) 1)
+          ((> column row) 0)
+          ((< column 0) 0)
+          ((+ (pascal-triangle (- row 1) (- column 1))
+          (pascal-triangle (- row 1) column )))
+    )
+)
+(provide pascal-triangle)
--- tests.rkt
+++ tests.rkt
@@ -2,12 +2,29 @@
 
 (require rackunit "exercises.rkt")
 
-;Exercise 01
+;Exercise 1.3.
 (check-equal? (sum-of-squares-of-top-two 1 2 3) 13)
 (check-equal? (sum-of-squares-of-top-two 3 4 2) 25)
 (check-equal? (sum-of-squares-of-top-two 5 3 4) 41)
 
-;Exercise 02
+;Exercise 1.10.
 (check-equal? (A 1 10) 1024)
 (check-equal? (A 2 4) 65536)
 (check-equal? (A 3 3) 65536)
+
+;Exercise 1.11. 
+(check-equal? (f 1) 1)
+(check-equal? (f 2) 2)
+(check-equal? (f 3) 4)
+(check-equal? (f 4) 11)
+(check-equal? (f 5) 25)
+(check-equal? (f 6) 59)
+(check-equal? (f 7) 142)
+
+;Exercise 1.12.
+(check-equal? (pascal-triangle 1 1) 1)
+(check-equal? (pascal-triangle 2 2) 1)
+(check-equal? (pascal-triangle 3 2) 2)
+(check-equal? (pascal-triangle 4 2) 3)
+(check-equal? (pascal-triangle 5 2) 4)
+(check-equal? (pascal-triangle 5 3) 6)
